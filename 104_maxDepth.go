1f29a2f29ef21e8b59fc8b5d9dc5b1c890c7eb8d
Artjom Plaunov <artyemnyc@gmail.com>
2020-12-01 19:25:25 -0500

    104

---
+++ 104_maxDepth.go
@@ -0,0 +1,18 @@
+import "math"
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func maxDepth(root *TreeNode) int {
+    if root == nil {
+        return 0
+    } else {
+        return 1 + int(math.Max(float64(maxDepth(root.Left)), float64(maxDepth(root.Right))))
+    }
+}
+
