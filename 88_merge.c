f4aa8bf42d36ac7b75ffb104321f073c65dbc229
Artjom Plaunov <artyemnyc@gmail.com>
2021-01-11 18:49:17 -0500

    88_merge.c

--- 88_merge.c
+++ 88_merge.c
@@ -1,16 +1,13 @@
 void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n){
-    int next    = m+n-1;
-    int one     = m-1;
-    int two     = n-1;
+    int next = (n--)+(m--)-1;
     
-    while (one >= 0 && two >= 0) {
-        if (nums1[one] > nums2[two]) 
-            nums1[next--] = nums1[one--];
+    while (m >= 0 && n >= 0) {
+        if (nums1[m] > nums2[n]) 
+            nums1[next--] = nums1[m--];
         else
-            nums1[next--] = nums2[two--];
+            nums1[next--] = nums2[n--];
     }
-    while (one >= 0) 
-        nums1[next--] = nums1[one--];
-    while (two >= 0) 
-        nums1[next--] = nums2[two--];
+    while (n >= 0) {
+        nums1[next--] = nums2[n--];
+    }
 }
